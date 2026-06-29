6a1da3eccc2b07d590ddef53a40c73a748d052f6
Ruslan Ataev <gumabemc@yandex.ru>
2026-06-29 17:22:56 +0300

    chore: delete unused imports

--- app/src/main/java/com/ruslanataev/razbudilnik/data/alarm/scheduler/AlarmSchedulerImpl.kt
+++ app/src/main/java/com/ruslanataev/razbudilnik/data/alarm/scheduler/AlarmSchedulerImpl.kt
@@ -1,14 +1,12 @@
 package com.ruslanataev.razbudilnik.data.alarm.scheduler
 
 import android.app.AlarmManager
-import android.app.AlarmManager.RTC_WAKEUP
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.os.Build
 import com.ruslanataev.razbudilnik.runtime.alarm.AlarmReceiver
 import com.ruslanataev.razbudilnik.domain.alarm.api.AlarmScheduler
-import com.ruslanataev.razbudilnik.presentation.ui.MainActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import java.time.LocalDateTime
 import java.time.ZoneId
