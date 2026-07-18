749ddd3b909818045c5468c98906cee64787a741
Ruslan Ataev <gumabemc@yandex.ru>
2026-07-18 17:49:04 +0300

    feat: reschedule alarm after it fires

--- app/src/main/java/com/ruslanataev/razbudilnik/runtime/alarm/AlarmReceiver.kt
+++ app/src/main/java/com/ruslanataev/razbudilnik/runtime/alarm/AlarmReceiver.kt
@@ -3,8 +3,18 @@ package com.ruslanataev.razbudilnik.runtime.alarm
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import com.ruslanataev.razbudilnik.domain.alarm.usecases.RescheduleEnabledAlarmUseCase
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class AlarmReceiver : BroadcastReceiver() {
+
+    @Inject
+    lateinit var rescheduleEnabledAlarmUseCase: RescheduleEnabledAlarmUseCase
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action != ACTION_TRIGGER_ALARM) {
@@ -21,6 +31,16 @@ class AlarmReceiver : BroadcastReceiver() {
                 minute = minute,
             ),
         )
+
+        val pendingResult = goAsync()
+
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                rescheduleEnabledAlarmUseCase()
+            } finally {
+                pendingResult.finish()
+            }
+        }
     }
 
     companion object {
