bb63d7ed45cf173c0d949fe845bfcdc0779cb199
pyclan88 <gumabemc@yandex.ru>
2026-07-19 17:48:48 +0300

    feat: synchronize direct boot alarm snapshot
    - update the snapshot after alarm time changes
    - update the snapshot after enabled state changes
    - keep snapshot coordination inside domain use cases

--- app/src/main/java/com/ruslanataev/razbudilnik/domain/setup/usecases/SaveAlarmEnabledUseCase.kt
+++ app/src/main/java/com/ruslanataev/razbudilnik/domain/setup/usecases/SaveAlarmEnabledUseCase.kt
@@ -1,12 +1,16 @@
 package com.ruslanataev.razbudilnik.domain.setup.usecases
 
+import com.ruslanataev.razbudilnik.domain.alarm.usecases.SynchronizeDirectBootAlarmSnapshotUseCase
 import com.ruslanataev.razbudilnik.domain.setup.api.AlarmSettingsRepository
 import javax.inject.Inject
 
 class SaveAlarmEnabledUseCase @Inject constructor(
     private val repository: AlarmSettingsRepository,
+    private val synchronizeDirectBootAlarmSnapshotUseCase: SynchronizeDirectBootAlarmSnapshotUseCase,
 ) {
     suspend operator fun invoke(enable: Boolean) {
         repository.saveEnabled(enable)
+
+        synchronizeDirectBootAlarmSnapshotUseCase()
     }
 }
--- app/src/main/java/com/ruslanataev/razbudilnik/domain/setup/usecases/SaveAlarmTimeUseCase.kt
+++ app/src/main/java/com/ruslanataev/razbudilnik/domain/setup/usecases/SaveAlarmTimeUseCase.kt
@@ -1,12 +1,16 @@
 package com.ruslanataev.razbudilnik.domain.setup.usecases
 
+import com.ruslanataev.razbudilnik.domain.alarm.usecases.SynchronizeDirectBootAlarmSnapshotUseCase
 import com.ruslanataev.razbudilnik.domain.setup.api.AlarmSettingsRepository
 import javax.inject.Inject
 
 class SaveAlarmTimeUseCase @Inject constructor(
     private val repository: AlarmSettingsRepository,
+    private val synchronizeDirectBootAlarmSnapshotUseCase: SynchronizeDirectBootAlarmSnapshotUseCase,
 ) {
     suspend operator fun invoke(hour: Int, minute: Int) {
         repository.saveTime(hour, minute)
+
+        synchronizeDirectBootAlarmSnapshotUseCase()
     }
 }
