b3aabb84bcfa760f20bf0998e96944f3ebc206fd
Ruslan Ataev <gumabemc@yandex.ru>
2026-07-20 15:43:37 +0300

    feat: update reader progress from interaction

--- app/src/main/java/com/ruslanataev/razbudilnik/presentation/ui/reader/viewmodel/ReaderViewModel.kt
+++ app/src/main/java/com/ruslanataev/razbudilnik/presentation/ui/reader/viewmodel/ReaderViewModel.kt
@@ -5,6 +5,7 @@ import com.ruslanataev.razbudilnik.domain.reader.models.ReaderChallenge
 import com.ruslanataev.razbudilnik.domain.reader.models.ReaderChallengeProgress
 import com.ruslanataev.razbudilnik.domain.reader.usecases.CreateInitialReaderChallengeProgressUseCase
 import com.ruslanataev.razbudilnik.domain.reader.usecases.GetReaderChallengeUseCase
+import com.ruslanataev.razbudilnik.domain.reader.usecases.UpdateReaderChallengeProgressUseCase
 import com.ruslanataev.razbudilnik.presentation.ui.reader.mappers.ReaderChallengeToReaderUiStateMapper
 import com.ruslanataev.razbudilnik.presentation.ui.reader.states.ReaderUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,15 +13,17 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
+import kotlin.time.Duration
 
 @HiltViewModel
 class ReaderViewModel @Inject constructor(
     private val getReaderChallengeUseCase: GetReaderChallengeUseCase,
     private val createInitialReaderChallengeProgressUseCase: CreateInitialReaderChallengeProgressUseCase,
+    private val updateReaderChallengeProgressUseCase: UpdateReaderChallengeProgressUseCase,
 ) : ViewModel() {
 
     private val challenge: ReaderChallenge = getReaderChallengeUseCase()
-    private val progress: ReaderChallengeProgress = createInitialReaderChallengeProgressUseCase()
+    private var progress: ReaderChallengeProgress = createInitialReaderChallengeProgressUseCase()
 
     private val _state: MutableStateFlow<ReaderUiState> = MutableStateFlow(
         ReaderChallengeToReaderUiStateMapper.map(
@@ -29,4 +32,22 @@ class ReaderViewModel @Inject constructor(
         ),
     )
     val state: StateFlow<ReaderUiState> = _state.asStateFlow()
+
+    fun onReadingInteractionTick(
+        elapsedTime: Duration,
+        isFingerDown: Boolean,
+        isFingerMoving: Boolean,
+    ) {
+        progress = updateReaderChallengeProgressUseCase(
+            progress = progress,
+            elapsedTime = elapsedTime,
+            isFingerDown = isFingerDown,
+            isFingerMoving = isFingerMoving,
+        )
+
+        _state.value = ReaderChallengeToReaderUiStateMapper.map(
+            challenge = challenge,
+            progress = progress,
+        )
+    }
 }
